7e01ec5439527f8339da97282fa0eecedc6b3f6b
tj <tjeakle15@gmail.com>
2014-12-17 08:01:06 -0600

    Removed logging and added begin rescue end to user test.

--- tests/user_test.rb
+++ tests/user_test.rb
@@ -9,7 +9,7 @@ class UserTest
   end
 
   def self.role_for_organization_multiple_test
-    #begin
+    begin
       user = User.new("test_user", nil)
       organization = Organization.new("TEST_ORGANIZATION", "Test Organization", nil, nil)
 
@@ -19,17 +19,15 @@ class UserTest
       user.organization_role_users = [organization_role_user_one, organization_role_user_two]
       organization.organization_role_users = [organization_role_user_one, organization_role_user_two]
 
-      puts user.role_for_organization(organization).class
-
       if user.role_for_organization(organization).eql?(Role::ADMIN)
         result = true
       else
         result = false
       end
 
-    #rescue
-    #  result = false
-    #end
+    rescue
+      result = false
+    end
     result
   end
 end
